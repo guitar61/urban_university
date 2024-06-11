d32c58f8a6f5cf84f28f62f59c28b3f359bf0750
guitar61 <161705817+guitar61@users.noreply.github.com>
2024-06-11 03:28:19 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,7 @@
+example_variable = "I love Python"
+print(example_variable[0])
+print(example_variable[-1])
+print(len(example_variable))
+print(example_variable[6:])
+print(example_variable[::-1])
+print(example_variable[::2])
