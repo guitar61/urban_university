2491f66ec8ec7e09e491fdcd8587f1937bc2bd9b
guitar61 <161705817+guitar61@users.noreply.github.com>
2024-06-12 01:47:11 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+numberOfCompletedTask = 12
+numberOfHours = 1.5
+courseName = "Python"
+timeForOneTask = numberOfHours/numberOfCompletedTask
+
+print(f" Course : {courseName}\n Total Task : {numberOfCompletedTask}\n Hour Spent : {numberOfHours}\n"
+      f" Average Completion Time : {timeForOneTask} Hours!")
