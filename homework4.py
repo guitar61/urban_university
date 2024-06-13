d2ff08899c29bfe9cabd96390f0397bf142981ac
guitar61 <161705817+guitar61@users.noreply.github.com>
2024-06-14 00:40:46 +0300

    Add files via upload
    Практическая работа по уроку "Организация программ и методы строк."

---
+++ homework4.py
@@ -0,0 +1,6 @@
+my_string = input("what is your full name and age ? ")
+
+print(len(my_string))
+
+print(f"{my_string.upper()}\n{my_string.lower()}\n{my_string.replace(' ', '')}\n"
+      f"{my_string[0]}\n{my_string[-1]}")
