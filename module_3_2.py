f78174271ee31268f1f2bbe1596af9589725be5d
Mahdi Gharehjeh Asl <161705817+guitar61@users.noreply.github.com>
2024-06-25 00:11:55 +0300

    "Способы вызова функции"

---
+++ module_3_2.py
@@ -0,0 +1,20 @@
+result1 = ('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = ('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+
+def single_root_words(root_word, *other_words):
+    same_words = []
+    root_word_lower = root_word.lower()
+
+    for word in other_words:
+        if word.lower() in root_word_lower:
+            same_words.append(word)
+        elif root_word_lower in word.lower():
+            same_words.append(word)
+
+    return same_words
+
+
+# unpacking the tuple as arguments inside the function.
+print(single_root_words(*result1))
+print(single_root_words(*result2))
