fe74fb266bffc5db2326f66ee820291bf37868ef
Mahdi Gharehjeh Asl <161705817+guitar61@users.noreply.github.com>
2024-06-28 00:05:59 +0300

    Update and rename module_3_2.py to module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -3,16 +3,14 @@ result2 = ('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 
 
 def single_root_words(root_word, *other_words):
-    same_words = []
-    root_word_lower = root_word.lower()
+same_words = []
+root_word_lower = root_word.lower()
 
-    for word in other_words:
-        if word.lower() in root_word_lower:
-            same_words.append(word)
-        elif root_word_lower in word.lower():
-            same_words.append(word)
+for word in other_words:
+if word.lower() in root_word_lower or root_word_lower in word.lower():
+same_words.append(word)
 
-    return same_words
+return same_words
 
 
 # unpacking the tuple as arguments inside the function.
