d4c752aeef0bf3a945356d00b04b17f56ae2a15d
Mahdi Gharehjeh Asl <161705817+guitar61@users.noreply.github.com>
2024-06-29 01:27:03 +0300

    Add files via upload
    "Рекурсия"

---
+++ module_3_5.py
@@ -0,0 +1,9 @@
+def get_multiplied_digits(num):
+    str_number = str(num)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+print(get_multiplied_digits(40203))
