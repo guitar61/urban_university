b6388e9455d15ebb287cc299fd3cfdcdbf553996
Mahdi Gharehjeh Asl <161705817+guitar61@users.noreply.github.com>
2024-07-01 16:27:27 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,10 +1,10 @@
 def print_params(a=1, b='string', c=True):
-    return a, b, c
+    print(a, b, c)
 
 
-print(print_params(b=25))
-print(print_params(c=[1, 2, 3]))
-print(print_params())
+print_params(b=25)
+print_params(c=[1, 2, 3])
+print_params()
 
 values_list = [False, 23, "help"]
 
@@ -12,10 +12,9 @@ values_dict = {'a': 'Alex',
                'b': 38,
                'c': 'male'
                }
-print(print_params(*values_list))
-print(print_params(**values_dict))
+print_params(*values_list)
+print_params(**values_dict)
 
 values_list_2 = [None, 34]
 
-print(print_params(*values_list_2, 42))
-
+print_params(*values_list_2, 42)
