26e230fd0c455647f1abaa7339dc0a696fc3df93
Mahdi Gharehjeh Asl <161705817+guitar61@users.noreply.github.com>
2024-07-30 22:01:59 +0300

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -1,5 +1,6 @@
 class Horse:
     def __init__(self, x_distance=0, sound='Frrr'):
+        super().__init__()
         self.x_distance = x_distance
         self.sound = sound
 
@@ -9,6 +10,7 @@ class Horse:
 
 class Eagle:
     def __init__(self, y_distance=0, sound='I train, eat, sleep, and repeat'):
+        super().__init__()
         self.y_distance = y_distance
         self.sound = sound
 
@@ -17,27 +19,28 @@ class Eagle:
 
 
 class Pegasus(Horse, Eagle):
-    def __init__(self, x_distance=0, y_distance=0, sound='Frrr'):
-        Horse.__init__(self, x_distance, sound)
-        Eagle.__init__(self, y_distance, 'I train, eat, sleep, and repeat')
+    def __init__(self, x_distance=0, y_distance=0):
+        super().__init__(x_distance=x_distance, sound='Frrr')
+        self.y_distance = y_distance
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def move(self, dx, dy):
         self.run(dx)
         self.fly(dy)
 
     def get_pos(self):
-        return (self.x_distance, self.y_distance)
+        return self.x_distance, self.y_distance
 
     def voice(self):
         print(self.sound)
 
 
+# Create an instance of Pegasus and test the methods
 p1 = Pegasus()
 
-print(p1.get_pos())
+print(p1.get_pos())  # Should print (0, 0)
 p1.move(10, 15)
-print(p1.get_pos())
+print(p1.get_pos())  # Should print (10, 15)
 p1.move(-5, 20)
-print(p1.get_pos())
-
-p1.voice()
+print(p1.get_pos())  # Should print (5, 35)
+p1.voice()  # Should print 'I train, eat, sleep, and repeat'
