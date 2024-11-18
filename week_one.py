8bce1efd79cf0aae8dfa7bf652bb7c1c8f73b9dc
Mahdi Gharehjeh Asl <161705817+guitar61@users.noreply.github.com>
2024-11-19 01:26:23 +0300

    Add files via upload

---
+++ week_one.py
@@ -0,0 +1,19 @@
+# 1st program
+print(9**0.5 * 5)
+
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+
+# 3rd program
+result1 = 2 * 2 + 2
+result2 = 2 * (2 + 2)
+print(result1, result2, result1 == result2)
+
+
+# 4th program
+number = float('123.456')  # Convert string to float
+shifted_number = int(number * 10)  # Shift decimal place and convert to integer
+first_digit = shifted_number % 10  # Get the first digit after the decimal
+print(first_digit)
