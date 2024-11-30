26e0fb686a9c66b5e54ff94a3e930eb15f5c4ee8
Mahdi Gharehjeh Asl <161705817+guitar61@users.noreply.github.com>
2024-11-30 13:37:35 +0300

    Update urls.py
    adding task4

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),           # Django Admin route
     path('task2/', include('task2.urls')),     # Routes for task2
     path('task3/', include('task3.urls')),     # Routes for task3
+    path('platform/', include('task4.urls')),  # Task4 included here
 ]
 
