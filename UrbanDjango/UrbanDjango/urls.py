bdf76d7db31ff5ef8c38c090bc12bf88c6e2f219
Mahdi Gharehjeh Asl <161705817+guitar61@users.noreply.github.com>
2024-12-01 03:05:47 +0300

    Update urls.py
    adding task5

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('task2/', include('task2.urls')),     # Routes for task2
     path('task3/', include('task3.urls')),     # Routes for task3
     path('platform/', include('task4.urls')),  # Task4 included here
+    path('task5/', include('task5.urls')),
 ]
 
