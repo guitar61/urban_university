c884732ce1f8f7275e41a4e388fd5005c6b61206
Mahdi Gharehjeh Asl <161705817+guitar61@users.noreply.github.com>
2024-12-05 03:13:17 +0300

    Update views.py

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 
-# View for the Main page
-def platform_view(request):
-    return render(request, 'third_task/platform.html')
-
-# View for the Games page
-def games_view(request):
-    return render(request, 'third_task/games.html')
-
-# View for the Cart page
-def cart_view(request):
-    return render(request, 'third_task/cart.html')
+def shop_page(request):
+    games = {
+        'game1': 'Minecraft',
+        'game2': 'The Witcher 3',
+        'game3': 'Cyberpunk 2077',
+    }
+    return render(request, 'third_task/shop.html', {'games': games})
