fd036e74174e0e2c426d935c4e4d6df07d1e5ac4
Mahdi Gharehjeh Asl <161705817+guitar61@users.noreply.github.com>
2024-12-11 22:41:24 +0300

    Add files via upload

--- module17/app/main.py
+++ module17/app/main.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
-from app.routers import task, user  # Import the routers for tasks and users
+from app.routers import user
 
-# Create FastAPI application instance
 app = FastAPI()
 
-# Root endpoint
-@app.get("/")
-def welcome_message():
-    return {"message": "Welcome to Taskmanager"}
-
-# Include routers from the "routers" folder
+# Include the user router
 app.include_router(user.router)
-app.include_router(task.router)
 
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to Task Manager"}
