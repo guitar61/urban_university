9ce8781bc6158bbef4d850d65a77b8820a6bd211
Mahdi Gharehjeh Asl <161705817+guitar61@users.noreply.github.com>
2024-12-15 01:51:38 +0300

    Add files via upload

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 
-def shop_page(request):
+
+# View for the Main page
+def platform_view(request):
+    return render(request, 'third_task/platform.html')
+
+
+# View for the Games page
+def games_view(request):
+    # Dictionary of games
     games = {
         'game1': 'Minecraft',
         'game2': 'The Witcher 3',
         'game3': 'Cyberpunk 2077',
     }
-    return render(request, 'third_task/shop.html', {'games': games})
+    return render(request, 'third_task/games.html', {'games': games})
+
+
+# View for the Cart page
+def cart_view(request):
+    return render(request, 'third_task/cart.html')
